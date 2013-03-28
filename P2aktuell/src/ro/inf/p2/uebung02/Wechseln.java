9335e1bd3db2a968f8fc4767494c6ebb32ad1f14
Felix <felix@stkn.org>
2013-03-28 23:58:55 +0100

    add wechseln

---
+++ P2aktuell/src/ro/inf/p2/uebung02/Wechseln.java
@@ -0,0 +1,19 @@
+package ro.inf.p2.uebung02;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: felix
+ * Date: 3/28/13
+ * Time: 11:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Wechseln {
+
+    public static final int[] werte =
+            new int[]{200, 100, 50, 20, 10, 5, 2, 1};
+
+    int[] anzahl(int betrag) {
+
+    }
+
+}
