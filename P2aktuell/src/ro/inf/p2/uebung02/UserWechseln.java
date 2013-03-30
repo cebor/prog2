6cafc1876633e853ea2649f0991de41d5659dcfd
Felix <felix@stkn.org>
2013-03-30 12:02:06 +0100

    some improvements

--- P2aktuell/src/ro/inf/p2/uebung02/UserWechseln.java
+++ P2aktuell/src/ro/inf/p2/uebung02/UserWechseln.java
@@ -1,19 +1,37 @@
 package ro.inf.p2.uebung02;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created with IntelliJ IDEA.
  * User: felix
  * Date: 3/29/13
  * Time: 10:34 AM
- * To change this template use File | Settings | File Templates.
+ * Cents to Coin Calculator.
  */
 public class UserWechseln {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        int[] array = Wechseln.anzahl(783);
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 0; i < 8; i++)
-            System.out.print(array[i] + " ");
+        System.out.println("Cents > Coins: Enter '0' to exit!");
 
+        int cents;
+
+        do {
+            System.out.print("Cents: ");
+            cents = Integer.parseInt(in.readLine());
+
+            if (cents != 0) {
+                int[] array = Wechseln.anzahl(cents);
+
+                for (int i = 0; i < 8; i++)
+                    System.out.print(array[i] + " ");
+
+                System.out.println();
+            }
+        } while (cents != 0);
     }
 }
--- P2aktuell/src/ro/inf/p2/uebung02/Wechseln.java
+++ P2aktuell/src/ro/inf/p2/uebung02/Wechseln.java
@@ -5,7 +5,7 @@ package ro.inf.p2.uebung02;
  * User: felix
  * Date: 3/28/13
  * Time: 11:41 PM
- * To change this template use File | Settings | File Templates.
+ * Wechseln
  */
 public class Wechseln {
 
