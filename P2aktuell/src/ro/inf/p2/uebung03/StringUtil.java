6fbdf4b600a6d903d06ba086b70b1281ff5fa6fc
Felix <felix@stkn.org>
2013-04-12 16:44:02 +0200

    add StringUtil

---
+++ P2aktuell/src/ro/inf/p2/uebung03/StringUtil.java
@@ -0,0 +1,22 @@
+package ro.inf.p2.uebung03;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: felix
+ * Date: 4/12/13
+ * Time: 11:44 AM
+ * StringUtil
+ */
+public class StringUtil {
+
+    public static char[] string2CharArray(String s) {
+
+        return s.toCharArray();
+    }
+
+    public static String charArray2String(char[] arr) {
+
+        return new String(arr);
+    }
+
+}
