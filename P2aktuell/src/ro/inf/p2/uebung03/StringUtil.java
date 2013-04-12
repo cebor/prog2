5f0fded37c759f017827d24749dae0d6462a99f3
Felix <felix@stkn.org>
2013-04-12 22:19:05 +0200

    add alternatives

--- P2aktuell/src/ro/inf/p2/uebung03/StringUtil.java
+++ P2aktuell/src/ro/inf/p2/uebung03/StringUtil.java
@@ -11,12 +11,26 @@ public class StringUtil {
 
     public static char[] string2CharArray(String s) {
 
-        return s.toCharArray();
+        char[] arr = new char[s.length()];
+
+        for (int i = 0; i < s.length(); i++)
+            arr[i] = s.charAt(i);
+
+        return arr;
+
+        // return s.toCharArray();  // alternative
     }
 
     public static String charArray2String(char[] arr) {
 
-        return new String(arr);
+        StringBuffer s = new StringBuffer(arr.length);
+
+        for (char entry : arr)
+            s.append(entry);
+
+        return s.toString();
+
+        // return new String(arr);  // alternative
     }
 
 }
